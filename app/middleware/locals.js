e05f8e1894000d3bab281198ebf37a79afd49883
mark <markharrop@yahoo.com>
2018-02-23 16:35:26 +0000

    :wrench: added webtrend, google, hotjar env settings

--- app/middleware/locals.js
+++ app/middleware/locals.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = config =>
   (req, res, next) => {
+    res.locals.GOOGLE_ANALYTICS_TRACKING_ID = config.googleAnalyticsId;
+    res.locals.WEBTRENDS_ANALYTICS_TRACKING_ID = config.webtrendsId;
+    res.locals.HOTJAR_ANALYTICS_TRACKING_ID = config.hotjarId;
     res.locals.SITE_ROOT = req.app.locals.SITE_ROOT;
     res.locals.ASSETS_URL = req.app.locals.ASSETS_URL;
     next();
