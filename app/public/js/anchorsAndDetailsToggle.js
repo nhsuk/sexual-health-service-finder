46ca9b25e60ee1347f9eb0ccb440f715fde99da2
Eva Barabas <b_seven_e@yahoo.co.uk>
2018-03-22 08:52:34 +0000

    :arrow_up: yarn upgrade

---
+++ app/public/js/anchorsAndDetailsToggle.js
@@ -0,0 +1,18 @@
+(function (global) {
+  'use strict';
+  var $ = global.jQuery;
+
+  if (window.location.hash !== "") {
+    var anchor = window.location.hash.substr(1);
+    var anchors = ['england', 'scotland', 'wales', 'northern'];
+    $.each(anchors, function (index, value) {
+      $('#' + value).removeAttr('open');
+    });
+    $('#' + anchor).attr('open', 'true');
+
+    $('html,body').animate({
+      scrollTop: $('#' + anchor).offset().top
+    });
+  }
+
+})(window);
