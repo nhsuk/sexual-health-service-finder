804ef7f7957baa65e44d022583157227bd220ce1
Eva Barabas <b_seven_e@yahoo.co.uk>
2018-04-03 17:03:59 +0100

    :construction_worker: build query for kits and 16 to 25, untangle professionals

--- test/unit/lib/utils/getFilteredOutTermsQuery.js
+++ test/unit/lib/utils/getFilteredOutTermsQuery.js
@@ -6,34 +6,6 @@ const getFilter = require('../../../../app/lib/utils/getFilteredOutTermsQuery');
 const expect = chai.expect;
 
 describe('getFilteredOutTermsQuery', () => {
-  const searchType = constants.searchTypes.sexperts;
-
-  it('should return mustClause for search type of sexual health professionals', () => {
-    const query = getFilter(searchType);
-    expect(query.mustClause)
-      .to.be.eql(
-        {
-          match: {
-            venueType: 'Clinic',
-          },
-        },
-        `"must clause found in\n${util.inspect(query, { depth: null })}`
-      );
-  });
-
-  it('should return mustNotClause for search type of sexual health professionals', () => {
-    const query = getFilter(searchType);
-    expect(query.mustNotClause)
-      .to.be.eql(
-        [
-          { match: { name: 'Marie Stopes' } },
-          { match: { name: 'Young People Friendly Practice' } },
-          { match: { name: 'BPAS' } },
-        ],
-        `"must not clause found in\n${util.inspect(query, { depth: null })}`
-      );
-  });
-
   it('should return undefined for empty searchType', () => {
     const query = getFilter(undefined);
     expect(query)
@@ -41,5 +13,108 @@ describe('getFilteredOutTermsQuery', () => {
         undefined,
         `"must_not clause not found in\n${util.inspect(query, { depth: null })}`
       );
+  });
+
+  describe('for sexperts', () => {
+    const searchType = constants.searchTypes.sexperts;
+
+    it('should return mustClause for search type of sexual health professionals', () => {
+      const query = getFilter(searchType);
+      expect(query.mustClause)
+        .to.be.eql(
+          [
+            { match: { type: 'Sexual health information and support' } },
+            { match: { venueType: 'Clinic' } },
+          ],
+          `"must clause found in\n${util.inspect(query, { depth: null })}`
+        );
+    });
+
+    it('should return mustNotClause for search type of sexual health professionals', () => {
+      const query = getFilter(searchType);
+      expect(query.mustNotClause)
+        .to.be.eql(
+          [
+            { match: { name: 'Marie Stopes' } },
+            { match: { name: 'Young People Friendly Practice' } },
+            { match: { name: 'BPAS' } },
+          ],
+          `"must not clause found in\n${util.inspect(query, { depth: null })}`
+        );
+    });
+  });
+
+  describe('for kits and 16 to 24', () => {
+    const searchType = constants.searchTypes.kits16to24;
+
+    it('should return should clause for search type of kits and ages 16 to 24', () => {
+      const query = getFilter(searchType);
+      expect(query.shouldClause)
+        .to.be.eql(
+          [
+            {
+              bool: {
+                must: [
+                  {
+                    match: {
+                      type: 'Sexual health information and support',
+                    },
+                  },
+                  {
+                    match: {
+                      venueType: 'Community',
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              bool: {
+                minimum_should_match: 1,
+                must: {
+                  match: {
+                    type: 'Chlamydia screening under 25s',
+                  },
+                },
+                should: [
+                  {
+                    match: {
+                      venueType: 'Clinic',
+                    },
+                  },
+                  {
+                    match: {
+                      venueType: 'Community',
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+          `"should clause found in\n${util.inspect(query, { depth: null })}`
+        );
+    });
+
+    it('should return should clause minimum for search type of kits and ages 16 to 24', () => {
+      const query = getFilter(searchType);
+      expect(query.minShouldMatch)
+        .to.be.eql(
+          1,
+          `"should min clause found in\n${util.inspect(query, { depth: null })}`
+        );
+    });
+
+    it('should return mustNotClause for search type of sexual health professionals', () => {
+      const query = getFilter(searchType);
+      expect(query.mustNotClause)
+        .to.be.eql(
+          [
+            { match: { name: 'Marie Stopes' } },
+            { match: { name: 'Young People Friendly Practice' } },
+            { match: { name: 'BPAS' } },
+          ],
+          `"must not clause found in\n${util.inspect(query, { depth: null })}`
+        );
+    });
   });
 });
