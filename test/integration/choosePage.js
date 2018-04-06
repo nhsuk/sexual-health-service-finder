dfda77cd1510af6eaea10b1f4b0b2e6bb88ecab6
Steven Hunt <st3v3n.hunt@gmail.com>
2018-04-06 09:31:39 +0100

    :one: Execute each variable request just once

--- test/integration/choosePage.js
+++ test/integration/choosePage.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const cheerio = require('cheerio');
-const app = require('../../server');
 const constants = require('../../app/lib/constants');
+const server = require('../../server');
 const iExpect = require('../lib/expectations');
 
 const expect = chai.expect;
@@ -11,65 +11,67 @@ chai.use(chaiHttp);
 
 const chooseRoute = `${constants.siteRoot}/choose`;
 
-function assertChooseResponse(age, done, assertions) {
-  chai.request(app)
-    .get(chooseRoute)
-    .query({ age })
-    .end((err, res) => {
-      expect(err).to.equal(null);
-      iExpect.htmlWith200Status(res);
-      assertions(err, res);
-      done();
-    });
-}
-
 describe('Choose page', () => {
-  const ageOver25 = constants.age.over25;
-  const ageUnder25 = constants.age['16to24'];
+  describe('for 16 to 24', () => {
+    let res;
+    before('run request', async () => {
+      res = await chai.request(server)
+        .get(chooseRoute)
+        .query({ age: constants.age['16to24'] });
+      iExpect.htmlWith200Status(res);
+    });
 
-  it('page title should be \'How do you want to get a test?\' if age question is answered 16-24', (done) => {
-    assertChooseResponse(ageUnder25, done, (err, res) => {
+    it('page title should be \'How do you want to get a test?\' if age question is answered 16-24', async () => {
       const $ = cheerio.load(res.text);
       expect($('.local-header--title--question').text()).to.equal('How do you want to get a test?');
     });
-  });
 
-  it('page options should be related to being under 25 (free), if age question is answered 16-24', (done) => {
-    assertChooseResponse(ageUnder25, done, (err, res) => {
+    it('page options should be related to being under 25 (free), if age question is answered 16-24', async () => {
       const $ = cheerio.load(res.text);
+
       expect($($('.multiple-choice .multiple--choice-option')[0]).text()).to.equal('Collect a free test kit near you');
     });
   });
 
-  it('page title should be \'How do you want to get a test?\' if age question is answered 25 or older', (done) => {
-    assertChooseResponse(ageOver25, done, (err, res) => {
+  describe('for over 25s', () => {
+    let res;
+    before('run request', async () => {
+      res = await chai.request(server)
+        .get(chooseRoute)
+        .query({ age: constants.age.over25 });
+      iExpect.htmlWith200Status(res);
+    });
+
+    it('page title should be \'How do you want to get a test?\' if age question is answered 25 or older', async () => {
       const $ = cheerio.load(res.text);
       expect($('.local-header--title--question').text()).to.equal('How do you want to get a test?');
     });
-  });
 
-  it('page options should be related to being over 25 (paid), if age question is answered 25 or older', (done) => {
-    assertChooseResponse(ageOver25, done, (err, res) => {
+    it('page options should be related to being over 25 (paid), if age question is answered 25 or older', async () => {
       const $ = cheerio.load(res.text);
+
       expect($($('.multiple-choice .multiple--choice-option')[0]).text()).to.equal('See a sexual health professional for free near you');
     });
   });
 
   describe('return to Choices services', () => {
-    it('the breadcrumb should have a link back to the Choices \'Services near you\'', (done) => {
-      assertChooseResponse({}, done, (err, res) => {
-        const $ = cheerio.load(res.text);
-        expect($($('div.breadcrumb a')[1]).attr('href'))
-          .to.equal('https://www.nhs.uk/service-search');
-      });
+    let res;
+    before('run request', async () => {
+      res = await chai.request(server).get(chooseRoute);
     });
 
-    it('the page should have a link back to the Choices service search', (done) => {
-      assertChooseResponse({}, done, (err, res) => {
-        const $ = cheerio.load(res.text);
-        expect($('.back-to-choices').attr('href'))
-          .to.equal('https://www.nhs.uk/service-search');
-      });
+    it('the breadcrumb should have a link back to the Choices \'Services near you\'', async () => {
+      const $ = cheerio.load(res.text);
+
+      expect($($('div.breadcrumb a')[1]).attr('href'))
+        .to.equal('https://www.nhs.uk/service-search');
+    });
+
+    it('the page should have a link back to the Choices service search', async () => {
+      const $ = cheerio.load(res.text);
+
+      expect($('.back-to-choices').attr('href'))
+        .to.equal('https://www.nhs.uk/service-search');
     });
   });
 });
