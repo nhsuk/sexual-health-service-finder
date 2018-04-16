d7461f2181e01e6b371ab997a8c1ed70ebaf5a8a
steven hunt <st3v3n.hunt@gmail.com>
2018-04-16 16:43:14 +0100

    :eyes: Simplify address formatter, use const in place of let

--- app/lib/utils/addressFormatter.js
+++ app/lib/utils/addressFormatter.js
@@ -3,10 +3,7 @@ const joinTruthyValues = require('./utils').joinTruthyValues;
 function addressFormatter(address) {
   if (!address || Object.keys(address).length === 0) { return undefined; }
 
-  let addressLines = [];
-  if (address.addressLines) {
-    addressLines = address.addressLines;
-  }
+  const addressLines = address.addressLines || [];
   if (address.postcode) {
     addressLines.push(address.postcode);
   }
