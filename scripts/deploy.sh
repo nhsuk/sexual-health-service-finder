88d19ff21dba87f99b02acd9c1cbfec4dbd6973d
leboemori <leboemori@users.noreply.github.com>
2019-01-18 16:07:30 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#removing CI script for azure pipeline might help isolate AZdevops task.
 
 git clone --depth 1 --single-branch --branch master https://github.com/nhsuk/ci-deployment.git scripts/ci-deployment
 
