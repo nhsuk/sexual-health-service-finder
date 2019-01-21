128af22af84fb079e4753598fd8fa05e596475e2
leboemori <leboemori@users.noreply.github.com>
2019-01-21 14:25:35 +0000

    comment out deploy.sh
    Trying to remove Travis dependency.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git clone --depth 1 --single-branch --branch master https://github.com/nhsuk/ci-deployment.git scripts/ci-deployment
+#git clone --depth 1 --single-branch --branch master https://github.com/nhsuk/ci-deployment.git scripts/ci-deployment
 
-bash scripts/ci-deployment/deploy.sh
+#bash scripts/ci-deployment/deploy.sh
