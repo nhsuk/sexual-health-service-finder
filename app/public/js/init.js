10d652a238a2c1ff015cbc86a65886f15d00c260
Tom Doughty <thomashdoughty@gmail.com>
2019-11-17 12:35:22 +0000

    :sparkles: Setup app to use nhsuk-frontend layout

--- app/public/js/init.js
+++ app/public/js/init.js
@@ -1,8 +1,1 @@
-// Require all modules needed for the application
-require('./anchorsAndDetailsToggle');
-require('./headerMobileMenuToggle');
-require('./headerMobileSearchToggle');
-require('./headerSearchTypeahead');
-require('./openToggle');
-require('./radioSelect');
-require('./skiplinkFix');
+console.log('bootstrapped');
--- config/nunjucks.js
+++ config/nunjucks.js
@@ -5,7 +5,11 @@ const log = require('../app/lib/logger');
 function configureNunjucks(app, config) {
   app.set('views', `${config.app.root}/app/views`);
   app.set('view engine', 'nunjucks');
-  const nunjucksEnvironment = nunjucks.configure(`${config.app.root}/app/views`, {
+  const appViews = [
+    `${config.app.root}/app/views`,
+    'node_modules/nhsuk-frontend/packages/components',
+  ];
+  const nunjucksEnvironment = nunjucks.configure(appViews, {
     autoescape: true,
     express: app,
     watch: true,
